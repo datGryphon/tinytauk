9ff6ec8b7c60f77f40259f14d0284e954c3c876f
Caleb <calebwastler@gmail.com>
2026-09-09 16:49:17 -0400

    feat: add Bean Flux2 INT8 quantization gate

--- src/tinytauk/quant/__init__.py
+++ src/tinytauk/quant/__init__.py
@@ -1,3 +1,15 @@
 from .config import QuantizationPolicy
+from .dynamic import (
+    apply_dynamic_int8_linears,
+    configure_x86_quantized_engine,
+    is_sensitive_generator_linear,
+    select_dynamic_int8_linears,
+)
 
-__all__ = ["QuantizationPolicy"]
+__all__ = [
+    "QuantizationPolicy",
+    "apply_dynamic_int8_linears",
+    "configure_x86_quantized_engine",
+    "is_sensitive_generator_linear",
+    "select_dynamic_int8_linears",
+]
