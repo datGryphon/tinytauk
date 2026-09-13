f7121dfcb0efd1cdff14a5d0ecded1413b339807
Caleb <calebwastler@gmail.com>
2026-09-13 15:15:20 -0400

    fix: export renamed quant selector

--- src/tinytauk/quant/__init__.py
+++ src/tinytauk/quant/__init__.py
@@ -2,12 +2,12 @@ from .dynamic import (
     apply_dynamic_int8_linears,
     configure_x86_quantized_engine,
     is_sensitive_generator_linear,
-    select_dynamic_int8_linears,
+    select_generator_quant_linears,
 )
 
 __all__ = [
     "apply_dynamic_int8_linears",
     "configure_x86_quantized_engine",
     "is_sensitive_generator_linear",
-    "select_dynamic_int8_linears",
+    "select_generator_quant_linears",
 ]
